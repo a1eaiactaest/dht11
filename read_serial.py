c5f31678488a91105ef47953404bb9be5f3d2278
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-09-08 20:53:37 +0200

    flask?

--- read_serial.py
+++ read_serial.py
@@ -3,19 +3,30 @@
 import serial
 import time
 import sys
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 port = sys.argv[1]
 s = serial.Serial(port, 9600)
 s.reset_input_buffer()
 
-while (1):
-  try: 
-    s_bytes = s.readline()
-    decoded = s_bytes[0:len(s_bytes)-2].decode("utf-8")
-    v = [float(x) for x in decoded.split(' ')]
-    print(v)
+@app.route('/')
+def hello():
+  return render_template('index.html')
 
-  except Exception as e:
-    print(e)
-    break
+def info():
+  while (1):
+    try: 
+      s_bytes = s.readline()
+      decoded = s_bytes[0:len(s_bytes)-2].decode("utf-8")
+      v = [float(x) for x in decoded.split(' ')]
+      print(v)
+
+    except Exception as e:
+      print(e)
+      break
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
