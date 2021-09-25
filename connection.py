0d949083999246235b378b10cd9a80ca9dc51a49
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-09-25 18:30:27 +0200

    sim is no longer a sim

--- connection.py
+++ connection.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-# SIM:
+# example:
 # [1.0, 1000.3, 124.23, 24.34, 61.57, 15.12, 71.3, 69.42, 42.07, 45.65, 0.0]
 
 import os
 DEBUG = os.getenv("DEBUG", None) is not None
-SIM = os.getenv("SIM", None) is not None
 import serial
 import time
 import sys
@@ -19,16 +18,10 @@ class Connection:
     self.s.reset_input_buffer()
 
     time.sleep(2) # sleep 2 seconds before reading, otherwise it bugs
-
-    self.f = open('data.json', 'a') # remove this in future
-    self.x = []
+    self.reset()
 
   def reset(self):
     self.x = []
-
-  def write(self, data):
-    self.f.write(data) 
-    self.f.write('\n')
 
   def serialize(self, d):
     self.x.append(d)
@@ -36,40 +29,32 @@ class Connection:
     #print(packed)
     return packed 
 
-  def read(self, DEBUG=False, SIM=True):
+  def read(self, DEBUG=False):
     s_bytes = self.s.readline()
     try:
-      if SIM:
-        bytes_decoded = s_bytes[0:len(s_bytes)-3].decode("utf-8")
-      else:
-        bytes_decoded = s_bytes[0:len(s_bytes)-2].decode("utf-8")
+      bytes_decoded = s_bytes[0:len(s_bytes)-3].decode("utf-8")
     except ValueError as e:
       print(e)
     val = [float(v) for v in bytes_decoded.split(" ")]
     ct = str(datetime.datetime.now())
     try:
-      if SIM:
-        ret = {"time": ct,
-               "id": val[0],
-               "pres": val[1],
-               "gas_res": val[2],
-               "a_temp": val[3],
-               "a_hum": val[4],
-               "gd_temp": val[5],
-               "gd_hum": val[6],
-               "gps_lat": val[7],
-               "gps_lon": val[8],
-               "gps_angle": val[9],
-               "gps_speed": val[10]}
-
-      else:
-        ret = {'time': ct, 'humidity': val[0], 'temp': val[1], 'hic': val[2]}
+      ret = {"time": ct,
+             "id": val[0],
+             "pres": val[1],
+             "gas_res": val[2],
+             "a_temp": val[3],
+             "a_hum": val[4],
+             "gd_temp": val[5],
+             "gd_hum": val[6],
+             "gps_lat": val[7],
+             "gps_lon": val[8],
+             "gps_angle": val[9],
+             "gps_speed": val[10]}
       if DEBUG:
         print(ret)
     except IndexError:
       print('error occured running again')
       return self.read() # don't know if works
-    self.write(self.serialize(ret))
     self.reset()
     return ret
 
@@ -80,5 +65,5 @@ if __name__ == "__main__":
     c = Connection(sys.argv[1])
   while(1):
     time.sleep(1)
-    c.read(True, SIM) 
+    c.read(True) 
 
