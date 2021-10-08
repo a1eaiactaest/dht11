09564a618754bd0512752b0b83f8332f08c4a1c7
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-10-08 13:15:30 +0200

    and this

--- db.py
+++ db.py
@@ -10,7 +10,8 @@ WRITE = os.getenv('WRITE', None) is not None
 CLEAR = os.getenv('CLEAR', None) is not None
 
 class Database:
-  def __init__(self):
+  def __init__(self, MODE):
+    self.MODE = MODE
     self.serial_connection = Connection('/dev/ttyACM0')
     self.conn = sqlite3.connect('serial_archive.db', check_same_thread=False)
     self.cur = self.conn.cursor() 
@@ -23,30 +24,30 @@ class Database:
       pass
 
   def init_db(self):
-    '''
-    data_table = """CREATE TABLE IF NOT EXISTS serial_data (
-                    time      text,
-                    id        integer, 
-                    pres      real,
-                    gas_res   real,
-                    a_temp    real,
-                    a_hum     real,
-                    gd_temp   real,
-                    gd_hum    real,
-                    gps_lat   real,
-                    gps_lon   real,
-                    gps_angle real,
-                    gps_speed real);"""
-    '''
-    data_table = """CREATE TABLE IF NOT EXISTS serial_data (
-                    time      text,
-                    id        integer, 
-                    pres      real,
-                    gas_res   real,
-                    a_temp    real,
-                    a_hum     real,
-                    gd_temp   real,
-                    gd_hum    real);"""
+    if self.MODE == "SIM":
+      data_table = """CREATE TABLE IF NOT EXISTS serial_data (
+                      time      text,
+                      id        integer, 
+                      pres      real,
+                      gas_res   real,
+                      a_temp    real,
+                      a_hum     real,
+                      gd_temp   real,
+                      gd_hum    real,
+                      gps_lat   real,
+                      gps_lon   real,
+                      gps_angle real,
+                      gps_speed real);"""
+    else:
+      data_table = """CREATE TABLE IF NOT EXISTS serial_data (
+                      time      text,
+                      id        integer, 
+                      pres      real,
+                      gas_res   real,
+                      a_temp    real,
+                      a_hum     real,
+                      gd_temp   real,
+                      gd_hum    real);"""
     self.create_table(data_table)
 
   def append_td(self, data: dict):
