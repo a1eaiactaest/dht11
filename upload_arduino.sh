2033a918488556380fea7d03734d25ef81da7c06
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-11-03 00:40:37 +0100

    duplicate files removal

--- upload_arduino.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# make changes in .ino file, run this
-# it takes some time to complete
-
-arduino-cli compile --fqbn arduino:avr:uno $1
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno $1
