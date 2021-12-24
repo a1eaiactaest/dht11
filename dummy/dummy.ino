ae27762fd4a28f3dfefc2b4b03f719d49f4ec2ca
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-12-24 11:19:20 +0100

    dummy error

--- dummy/dummy.ino
+++ dummy/dummy.ino
@@ -1,6 +1,9 @@
 // send dummy data
 int stations[3] = {3,5,11};
 
+// send error message once in while ~every 10th line
+String error_msg = "Error: Unexpected value inside of message. Data has been dropped."
+
 void setup(){
   Serial.begin(9600);
 }
@@ -18,14 +21,21 @@ void loop(){
   float gd_temp = random(0, 10);
   float gd_hum = random(60, 100);
 
-  float val_array[] = {pres, gas_res, a_temp, a_hum, gd_temp, gd_hum};
-  int n = sizeof(val_array)/sizeof(float);
 
-  Serial.print(id);
-  Serial.print(" ");
-  for (int i=0; i<n; i++){
-    Serial.print(val_array[i]);
+  int is_error = random (0, 10);
+
+  if (is_error == 5){
+    Serial.println(error_msg);
+  } else {
+    float val_array[] = {pres, gas_res, a_temp, a_hum, gd_temp, gd_hum};
+    int n = sizeof(val_array)/sizeof(float);
+
+    Serial.print(id);
     Serial.print(" ");
+    for (int i=0; i<n; i++){
+      Serial.print(val_array[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
-  Serial.println();
 }
