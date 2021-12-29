d12651011d82b527f2f319d30b4272dad093dce5
Piotrek Rybiec <piotrrybiec28@gmail.com>
2021-12-29 11:37:52 +0100

    is this equivalent?

--- tools/find_port.sh
+++ tools/find_port.sh
@@ -3,10 +3,8 @@
 # doesnt search /dev/cu.usbserial-A50285BI - chinese arduino uno
 
 if [ `uname` == "Darwin" ]; then
-  if  ls /dev/tty.usbmodem* 1> /dev/null 2>&1; then
-    SERIAL=`ls -f /dev/tty.usbmodem*`
-  elif ls /dev/tty.usbserial* 1> /dev/null 2>&1; then
-    SERIAL=`ls -f /dev/tty.usbserial*`
+  if  ls /dev/tty.usb* 1> /dev/null 2>&1; then
+    SERIAL=`ls -f /dev/tty.usb*`
   elif ls /tmp/ttyRERETX 1> /dev/null 2>&1; then
     SERIAL=`ls -f /tmp/ttyRERERX` 
   else 
