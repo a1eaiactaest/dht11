07287b45793b47a2ebd5bfaf2a57f2779c9d12e6
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-05 21:47:32 +0100

    fix, remove submodule

--- rere/src/App.jsx
+++ rere/src/App.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react"
 
 export default function App(){
   const [resp, setResp] = useState({})
+  const [error, setError] = useState(false)
   useEffect(() => {
     fetch("http://127:0.0.1:1337/api/info")
       .then(res => res.json())
@@ -10,21 +11,21 @@ export default function App(){
       })
       .catch(error => {
         console.log("cant fetch api!")
-        return (
-          <>
-            <p>Can't fetch from the API!</p>
-          </>
-        )
+        setError(true)
       })
   },[])
 
   return (
     <>
-      <p>time: {resp.time}</p>
-      <p>stations: {resp.id}</p>
-      <p>temperature: {resp.temp}</p>
-      <p>humidity: {resp.hum}</p>
-      <p>gas: {resp.gas}</p>
+      {error ? <p>Cant fetch from the API!</p> : 
+        <>
+          <p>time: {resp.time}</p>
+          <p>stations: {resp.id}</p>
+          <p>temperature: {resp.temp}</p>
+          <p>humidity: {resp.hum}</p>
+          <p>gas: {resp.gas}</p>
+        </>
+      }
     </>
   )
 }
