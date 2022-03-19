e024d35e7bab8693fb10577cfed59627a9b26b82
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-19 14:51:29 +0100

    note

--- tools/serial/connect.sh
+++ tools/serial/connect.sh
@@ -5,6 +5,7 @@ if ls $PORT 2> /dev/null; then
   sudo screen $PORT 9600
 fi
 
+# this executes after deattaching from a screen session.
 for scr in $(sudo screen -ls | awk '{print $1}'); do 
   sudo screen -S $scr -X kill; 
 done
