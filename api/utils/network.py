166887902c90452dc4b326f027c2456673c04d7a
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-19 15:44:41 +0100

    some tools

--- api/utils/network.py
+++ api/utils/network.py
@@ -1,20 +1,25 @@
+import os
 import re
 import requests
-from db import Database
 
-def init_db():
-  db = Database()
-  return db
+API_HOST = os.getenv("API_HOST", "http://localhost:1337")
 
-def url_is_up(url, title):
+def is_up(url, title):
   try:
     src_code = str(requests.get(url).content)
-    if re.search(f'<title>{title}', src_code):
+    if re.search(f"<title>{title}", src_code):
       return True
     return False
   except requests.exceptions.ConnectionError:
     # Couldn't resolve connection
     return False
 
+def api_get(endpoint, method="GET", timeout=None, **params):
+  headers = {}
+  headers["User-Agent"] = "rere-util"
+  return requests.request(method, f"{API_HOST}/{endpoint}", timeout=timeout, headers=headers, params=params)
+
 if __name__ == "__main__":
-  print(url_is_up("http://localhost:5000/", "RERE"))
+  #print(is_up("http://localhost:5000/", "RERE"))
+  ret = api_get("api/info", timeout=2)
+  print(acc, ret, ret.text)
