9f7edfbbe0e8e6182def50f93b1a0725eede47e6
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-04-12 22:43:16 +0200

    frontend: continue integration, stations route

---
+++ api/README.md
@@ -0,0 +1,19 @@
+## API
+
+Run the API:
+
+```bash
+./app.py 
+```
+
+API endpoints are `0.0.0.0:1337/api/*`, and are served with wsgi production server.
+
+Set the `DEBUG` environment variable to see more detailed output. Example: 
+
+```bash
+DEBUG=1 ./app.py 
+```
+
+Endpoints doesn't change. Flask is the server though.
+
+
--- frontend/src/routes/stations.js
+++ frontend/src/routes/stations.js
@@ -1,7 +1,42 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import Header from "../shared/components/Header";
+
+function StationList(props) {
+  const stations = props.stations;
+
+  const listStations = stations?.map((station) => 
+      <li key={station}><Link to={`/stations/${station}`}>Station <b>{station}</b></Link></li>
+  );
+
+  return(
+    <ul>
+      <li key={0}><Link to={'/stations/0'}>All Stations</Link></li>
+      {listStations}
+    </ul>
+  );
+};
+
 function Stations() {
+  const [stations, setStations] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:1337/api/stations_index") 
+      .then(res => res.json())
+      .then((res) => {
+        setStations(res)
+      })
+      .catch(error => {
+        console.log('Request to http://localhost:1337/api/stations_index failed.')
+      })
+  }, []);
+
+  console.log(stations);
   return(
     <main>
-      <h1>Stations</h1>
+      <Header className="text-3xl font-bold " label="Stations"/>
+      <StationList stations={stations} />
     </main>
   );
 };
