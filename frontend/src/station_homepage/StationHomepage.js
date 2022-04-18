fdabdba5ad6adb91adcf50a1700812450d956c7a
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-04-18 14:40:13 +0200

    frontend: change header if stations is 0

--- frontend/src/station_homepage/StationHomepage.js
+++ frontend/src/station_homepage/StationHomepage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Header from '../shared/components/Header';
 
 function StationHomepage(){
   const { station } = useParams();
+  const [allStations, setAllStations] = useState(false);
+
+  useEffect((station) => {
+    if (station === 0){
+      setAllStations(true);
+    };
+  },[]);
+
   return (
     <main>
-      <Header label={`Station: ${station} dashboard.`} />
+      <Header label={
+        allStations ? "All stations dashboard."
+        : `Station ${station} dashboard.`
+      } />
     </main>
   );
 };
